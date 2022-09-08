b0396d52568825b5e766788c714a195d3c9f2db3
Indigo <indigo.crandell@gmail.com>
2022-09-08 16:28:09 -0400

    Moved the prompt function into Employee, out of index.

--- Develop/Lib/Employee.js
+++ Develop/Lib/Employee.js
@@ -1,3 +1,6 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 class Employee {
     // first, the constructor
     constructor(name, id, email) {
@@ -37,6 +40,44 @@ class Employee {
             return undefined;
         }
     }
-}
+    
+    // Prompt user for manager info to get started
+    promptUser() {
+        inquirer
+            .prompt ([
+                {
+                    type: 'input',
+                    name: 'managerName',
+                    message: "Please enter manager's name."
+                },
+                {
+                    type: 'input',
+                    name: 'managerId',
+                    message: "Please enter manager's ID."
+                },
+                {
+                    type: 'input',
+                    name: 'managerEmail',
+                    message: "Please enter manager's email address."
+                },
+                {
+                    type: 'input',
+                    name: 'officeNumber',
+                    message: "Please enter office number."
+                },
+                {
+                    type: 'list',
+                    name: 'options',
+                    message: 'Please select an option:',
+                    choices: ['Add an engineer', 'Add an intern', 'Finish building my team']
+                }
+            ])
+    }
+};
+
+
+const elvis = new Employee("Elvis", 556, 'elvispres@hotmail.com');
+
+console.log(elvis);
 
 module.exports = Employee;
--- Develop/index.js
+++ Develop/index.js
@@ -1,47 +1,9 @@
-// Include required modules
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateHTML = require('./Src/generateHTML')
+// Require the parent constructor
 const Employee = require('./Lib/Employee');
-const Engineer = require('./Lib/Engineer');
-const Intern = require('./Lib/Intern');
-const Manager = require('./Lib/Manager');
 
-const promptUser = () =>
-inquirer
-    .prompt ([
-        {
-            type: 'input',
-            name: 'managerName',
-            message: "Please enter manager's name."
-        },
-        {
-            type: 'input',
-            name: 'managerId',
-            message: "Please enter manager's ID."
-        },
-        {
-            type: 'input',
-            name: 'managerEmail',
-            message: "Please enter manager's email address."
-        },
-        {
-            type: 'input',
-            name: 'officeNumber',
-            message: "Please enter office number."
-        },
-        {
-            type: 'list',
-            name: 'options',
-            message: 'Please select an option:',
-            choices: ['Add an engineer', 'Add an intern', 'Finish building my team']
-        }
-    ])
+// create a new employee to initialize app
+const employee = new Employee();
 
-    // Function to initialize app
-    function init() {
-        promptUser();
-    }
-    
-    // Run init
-    init();
+// A method from Employee.js
+employee.promptUser();
+
