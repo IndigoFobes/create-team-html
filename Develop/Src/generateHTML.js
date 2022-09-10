a7571bf8adc22c09e49fdd81c65b0d4819edf208
Indigo <indigo.crandell@gmail.com>
2022-09-10 09:10:04 -0400

    Working thorugh template literal. I don't think it has to be much for the homepage.

--- Develop/Src/generateHTML.js
+++ Develop/Src/generateHTML.js
@@ -3,8 +3,29 @@
 // 2. Create templates for each employee type
 
 // function to export
+// There will be parameters to input
 const generateHTML = () => {
-console.log('hello');
-}
+return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>My Team Homepage</h1>
+    <!-- For dynamically created boxes -->
+    <container id = "home-cont"></container>
+</body>
+</html>
+`
+};
+ 
+const generateEmployee = () => {
 
+};
+
+generateHTML();
 module.exports = generateHTML;
